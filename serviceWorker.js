7701b55a2153d1bdffe6e97cfbbf0c4277590495
Yadunandan Batchu <nandubatchu@gmail.com>
2025-05-13 07:00:10 +0000

    setup manifest

---
+++ serviceWorker.js
@@ -0,0 +1,21 @@
+// Basic service worker code
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('pwa-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'manifest.json',
+        // Add other essential files here
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
